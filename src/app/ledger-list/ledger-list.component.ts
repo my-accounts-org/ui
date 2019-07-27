a639a31fc539bd5eb013cabe5222a5b33859a92a
pandav14 <viva17j@gmail.com>
2019-07-27 18:46:36 +0530

    lint fix

--- src/app/ledger-list/ledger-list.component.ts
+++ src/app/ledger-list/ledger-list.component.ts
@@ -1,15 +1,15 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {CompanyModel} from "../models/company.model";
-import {MatTableDataSource} from "@angular/material/table";
-import {LedgerModel} from "../models/ledger.model";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatDialog, MatDialogConfig, MatSort} from "@angular/material";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {AccountsConstants} from "../shared/accounts.constants";
-import {Ng4LoadingSpinnerService} from "ng4-loading-spinner";
-import {LedgerService} from "../shared/ledger.service";
-import {CompanyComponent} from "../master/company/company.component";
-import {LedgerComponent} from "../master/ledger/ledger.component";
+import {CompanyModel} from '../models/company.model';
+import {MatTableDataSource} from '@angular/material/table';
+import {LedgerModel} from '../models/ledger.model';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatDialog, MatDialogConfig, MatSort} from '@angular/material';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {AccountsConstants} from '../shared/accounts.constants';
+import {Ng4LoadingSpinnerService} from 'ng4-loading-spinner';
+import {LedgerService} from '../shared/ledger.service';
+import {CompanyComponent} from '../master/company/company.component';
+import {LedgerComponent} from '../master/ledger/ledger.component';
 
 @Component({
   selector: 'app-ledger-list',
